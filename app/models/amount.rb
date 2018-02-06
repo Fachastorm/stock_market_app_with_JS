0fc54b0d77c61d4487a5cffd5f50f20f6f15f4d5
Fachastorm <rodrigo.lodeiro@live.com>
2018-02-06 09:52:48 -0500

    added writer

--- app/models/amount.rb
+++ app/models/amount.rb
@@ -2,7 +2,7 @@ class Amount < ApplicationRecord
     belongs_to :watchlist
 
     def total #Add the total amount of stock shares that have been bought on the site and route them. 
-        self.sum(:quantity)
+        @most_added = Amount.group(:watchlist_id).select('SUM(quantity) as qty').order('qty desc')
     end 
 
 end
