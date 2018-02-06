8fa915d5e13c317511a97e70e4d7bd5fa15a380f
Fachastorm <rodrigo.lodeiro@live.com>
2018-02-06 14:37:39 -0500

    finally complete

--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,8 +1,8 @@
 class Amount < ApplicationRecord 
     belongs_to :watchlist
 
-    def total #Add the total amount of stock shares that have been bought on the site and route them. 
-        @most_added = Amount.group(:watchlist_id).select('SUM(quantity) as qty').order('qty desc')
-    end 
-
+    def total #Add the total amount of stock shares that have been bought on the site and route them.
+         @value = Amount.group(:stock_name).sum(:quantity).to_a
+         @value[1..5]
+    end
 end
