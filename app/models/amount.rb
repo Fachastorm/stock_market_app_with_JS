7d3d0a6bf8cda2a41d013b5abaea349486df1251
Fachastorm <rodrigo.lodeiro@live.com>
2018-02-06 03:20:30 -0500

    added nested form writer for test

---
+++ app/models/amount.rb
@@ -0,0 +1,3 @@
+class Amount < ApplicationRecord 
+    belongs_to :watchlist
+end
--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,8 +1,17 @@
 class Watchlist < ApplicationRecord 
     belongs_to :user 
+    has_many :amounts
 
     validates :name, presence: true
     validates :description, presence: true
 
 
+
+    def amounts_attributes=(amounts_attributes)
+        amounts_attributes.each do |i, amount_attributes|
+          if self.errors.messages.empty?
+            self.amounts.build(amount_attributes)
+          end
+        end
+      end
 end 
