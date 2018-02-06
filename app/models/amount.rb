2b8dd888f6e2870d74bc3053195cdcf90d67f468
Fachastorm <rodrigo.lodeiro@live.com>
2018-02-06 03:23:58 -0500

    added nested form writer for test

--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,3 +1,8 @@
 class Amount < ApplicationRecord 
     belongs_to :watchlist
+
+    def total #Add the total amount of stock shares that have been bought on the site and route them. 
+        self.sum(:quantity)
+    end 
+
 end
